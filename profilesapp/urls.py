2301e68e0858c218bfcdeae8f0db58ccbd227a9c
diyajaiswal11 <shubhijaiswal2000@gmail.com>
2020-06-18 20:02:19 +0530

    completed

--- profilesapp/urls.py
+++ profilesapp/urls.py
@@ -3,21 +3,10 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
 
-"""
-router=DefaultRouter() 
-router.register('userdetail',views.UserDetailViewSet,basename='userdetail')
 
-
-detail_list_view=views.UserDetailViewSet.as_view(
-    {'get':'list'}
-)
-"""
 urlpatterns = [
-    #path('',include(router.urls)),
     path('',views.profile,name='profile'),
-    #path('',detail_list_view),
     path('user_detail/',views.user_detail,name='user_detail'),
     path('favourite/',views.favourite,name='favourite'),
     path('loginreg/',views.loginreg,name='loginreg'),
-    #path('userdetail/',views.userdetail,name='userdetail'),
 ]
